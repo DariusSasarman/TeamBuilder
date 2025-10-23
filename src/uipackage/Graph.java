e2ec5ad50c26ed7261fe73971e29753c9486f1a3
DariusSasarman <dariussasarman@gmail.com>
2025-10-23 20:40:38 +0300

    Added a placeholder image in place of the graph area.

--- src/uipackage/Graph.java
+++ src/uipackage/Graph.java
@@ -2,14 +2,41 @@ package uipackage;
 
 import javax.swing.*;
 import java.awt.*;
+import javax.swing.*;
+import java.awt.*;
+import java.net.URL;
 
 public class Graph extends JPanel {
+
+    private static final Image PLACEHOLDER_IMAGE;
+
+    static {
+        URL imageUrl = Graph.class.getResource("/icons/placeholderGraph.png");
+        Image image = null;
+        if (imageUrl != null) {
+            image = new ImageIcon(imageUrl).getImage();
+        } else {
+            System.err.println("Placeholder image not found.");
+        }
+        PLACEHOLDER_IMAGE = image;
+    }
+
+    public Graph() {
+
+    }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.clearRect(0,0,1000,1000);
-        g.setColor(new Color(255,255,255));
-        g.drawString(new String("No graph added yet"),50, 50);
+
+        if (PLACEHOLDER_IMAGE != null) {
+            int panelWidth = getWidth();
+            int panelHeight = getHeight();
+            g.drawImage(PLACEHOLDER_IMAGE, 0, 0, panelWidth, panelHeight, this);
+        } else {
+            g.clearRect(0, 0, getWidth(), getHeight());
+            g.setColor(Color.RED);
+            g.drawString("Image not loaded.", 50, 50);
+        }
     }
 }
